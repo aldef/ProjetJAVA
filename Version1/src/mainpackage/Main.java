b544772f3923c6d9cf85fdb32704874077ebccd9
alexandredef <alexandre.defrene@gmx.fr>
2023-01-30 14:12:39 +0100

    Version1 mise à jour

--- Version1/src/mainpackage/Main.java
+++ Version1/src/mainpackage/Main.java
@@ -7,41 +7,40 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    ArrayList<Livre> books = new ArrayList<Livre>();
-    ArrayList<Lecteur> readers = new ArrayList<Lecteur>();
 
     public static void main(String[] args) {
         // Initialisation des listes
-        ArrayList<Livre> books = new ArrayList<Livre>();
-        ArrayList<Lecteur> readers = new ArrayList<Lecteur>();
+        ArrayList<Livre> livres = new ArrayList<Livre>();
+        ArrayList<Lecteur> lecteurs = new ArrayList<Lecteur>();
 
         // Saisie des livres
         System.out.println("Saisie des livres : ");
+        System.out.print("Entrer le titre : ");
         Scanner scanner = new Scanner(System.in);
         String titre = scanner.nextLine();
         while (!titre.equals("")) {
-            System.out.println("Entrer l'auteur : ");
+            System.out.print("Entrer l'auteur : ");
             String auteur = scanner.nextLine();
-            books.add(new Livre(titre, auteur));
-            System.out.println("Entrer le titre : ");
+            livres.add(new Livre(titre, auteur));
+            System.out.print("Entrer le titre : ");
             titre = scanner.nextLine();
         }
 
         // Saisie des lecteurs
         System.out.println("Saisie des lecteurs : ");
-        System.out.println("Entrer le nom : ");
+        System.out.print("Entrer le nom : ");
         String nom = scanner.nextLine();
         while (!nom.equals("")) {
-            System.out.println("Entrer le prenom : ");
+            System.out.print("Entrer le prenom : ");
             String prenom = scanner.nextLine();
-            readers.add(new Lecteur(nom, prenom));
-            System.out.println("Entrer le nom : ");
+            lecteurs.add(new Lecteur(nom, prenom));
+            System.out.print("Entrer le nom : ");
             nom = scanner.nextLine();
         }
 
         // Affichage des listes
-        System.out.println("Livres : " + books);
-        System.out.println("Lecteurs : " + readers);
+        System.out.println("Livres : " + livres);
+        System.out.println("Lecteurs : " + lecteurs);
     }
 
     }
